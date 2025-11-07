d17b0e2a88cca8f1e5bdce5739ff91e63e8b59c7
Hupewolf <nhannguyen.ix5@gmail.com>
2025-11-07 21:05:27 +0700

    challenge4

---
+++ main.js
@@ -0,0 +1,25 @@
+// chọn tất cả ảnh trong gallery
+const galleryImgs = document.querySelectorAll('.gallery img');
+
+// tạo phần modal động
+const modal = document.createElement('div');
+modal.classList.add('modal');
+document.body.appendChild(modal);
+
+const modalImg = document.createElement('img');
+modal.appendChild(modalImg);
+
+// khi click ảnh -> hiển thị modal
+galleryImgs.forEach(img => {
+    img.addEventListener('click', () => {
+        modalImg.src = img.src;
+        modal.style.display = 'flex';
+    });
+});
+
+// khi click ra ngoài ảnh -> đóng modal
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
